c1a516c2e78294246cf89546827318e77f43e18e
VeronLo <ramisveron.lorena@gmail.com>
2024-11-27 21:20:17 -0300

    PFO3-DWSF-Ramis_Veron_Lorena

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+console.log('Ejercicio 1:');
+
+
 function calcularAreaRectangulo(length, width) {
     return length * width;
 }
 
+console.log('Ejemplo 1:');
 console.log(calcularAreaRectangulo(5, 3)); // Salida: 15
 
+console.log('Ejemplo 2:');
 console.log(calcularAreaRectangulo(10, 2)); // Salida: 20
 
+console.log('Ejemplo 3:');
 console.log(calcularAreaRectangulo(7, 4));  // Salida: 28
 separadorDeSolucion();
 /*Primero pensé en cómo representar la información y cómo realizar el cálculo.
@@ -14,14 +20,19 @@ Escribí el código utilizando la función que creé y llamé a esta función de
 Por último, ejecuté el código en un entorno de desarrollo y obtuve los resultados esperados.*/
 
 
+console.log('Ejercicio 2:');
 
 function contarPalabras(cadena) {
     let palabras = cadena.split(' ');
     return palabras.length;
 }
-
+console.log('Ejemplo 1:');
 console.log(contarPalabras("Humahuaca es un lugar copado")); // Resultado: 5
+
+console.log('Ejemplo 2:');
 console.log(contarPalabras("Me gustaría vivir en las montañas")); // Resultado: 6
+
+console.log('Ejemplo 3:');
 console.log(contarPalabras("¡hey! ¿hay alguien alli?")); // Resultado: 4
 separadorDeSolucion();
 /*Para contar las palabras en una cadena, primero divido la cadena en un array de palabras utilizando el método split(' '). 
@@ -29,15 +40,20 @@ Luego, devuelvo el número de elementos en este array utilizando el método leng
 Luego, escribí el código utilizando la función que creé y y llamé a esta función dentro de una declaración console.log para mostrar el resultado en la consola.*/
 
 
+console.log('Ejercicio 3:');
 
 function invertirCadena(cadena) {
     
     return cadena.split('').reverse().join('');
 }
 
-
+console.log('Ejemplo 1:');
 console.log(invertirCadena("hola")); // Resultado: "aloh"
+
+console.log('Ejemplo 2:');
 console.log(invertirCadena("tarolas")); // Resultado: "salorat"
+
+console.log('Ejemplo 3:');
 console.log(invertirCadena("Loquillo")); // Resultado: "olliuqol"
 separadorDeSolucion();
 /*Para invertir una cadena, primero divido la cadena en un array de caracteres utilizando el método split(' '). 
@@ -46,40 +62,49 @@ Finalmente, se vuelve a convertir el array de caracteres en una cadena utilizand
 Luego, escribí el código utilizando la función que creé y y llamé a esta función dentro de una declaración console.log para mostrar el resultado en la consola.*/
 
 
+console.log('Ejercicio 4:');
 
 function esPalindromo(cadena) {
     let cadenaLimpia = cadena.toLowerCase().replace(/\s+/g, '');
     let cadenaInvertida = cadenaLimpia.split('').reverse().join('');
     return cadenaLimpia === cadenaInvertida;
 }
+console.log('Ejemplo 1:');
 
 console.log(esPalindromo("neuquen")); // true
+
+console.log('Ejemplo 2:');
 console.log(esPalindromo("reconocer")); // true
-console.log(esPalindromo("rallar")); // true
+
+console.log('Ejemplo 3:');
 console.log(esPalindromo("moderno")); // false
 separadorDeSolucion();
 /*Para verificar si una cadena es un palíndromo, primero limpio la cadena de espacios en blanco y convertí la cadena a minúsculas utilizando los métodos toLowerCase() y replace(/\s+/g, ''). 
 Luego, invierto la cadena utilizando el método split('').reverse().join(''). 
 Finalmente, comparo la cadena limpia con la cadena inverttida*/
 
+
+console.log('Ejercicio 5:');
+
+console.log('Ejemplo 1:');
 function edadCanina() {
     let edadPerro = prompt("Introduce la edad de tu perro:");
     let edadHumana = edadPerro * 7;
     console.log("Tu perro tiene " + edadHumana + " años humanos");
 }
 edadCanina(); // Tu perro tiene .. años humanos
-separadorDeSolucion();
 
 
+console.log('Ejemplo 2:');
 function edadCanina() {
     let edadPerro = prompt("Introduce la edad de tu perro:");
     let edadHumana = edadPerro * 7;
     console.log("Tu perro tiene " + edadHumana + " años humanos");
 }
 edadCanina(); // Tu perro tiene .. años humanos
-separadorDeSolucion();
 
 
+console.log('Ejemplo 3:');
 function edadCanina() {
     let edadPerro = prompt("Introduce la edad de tu perro:");
     let edadHumana = edadPerro * 7;
